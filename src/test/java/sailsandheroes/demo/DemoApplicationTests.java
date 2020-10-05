c0a1498a3f7b1b2078ec8cdac0f999767076da63
Omoezone <30931803+Omoezone@users.noreply.github.com>
2020-10-05 13:25:17 +0200

    Initial commit

---
+++ src/test/java/sailsandheroes/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package sailsandheroes.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
