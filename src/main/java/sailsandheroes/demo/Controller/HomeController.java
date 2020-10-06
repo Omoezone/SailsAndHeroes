c4ad7794db77b0a0277a2bf4f9ca6ea18bc40004
BenjiHasCode <57215161+BenjiHasCode@users.noreply.github.com>
2020-10-06 11:04:13 +0200

    Html til poly

--- src/main/java/sailsandheroes/demo/Controller/HomeController.java
+++ src/main/java/sailsandheroes/demo/Controller/HomeController.java
@@ -1,13 +1,16 @@
 package sailsandheroes.demo.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import sailsandheroes.demo.Model.Hex;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("Hex", new Hex(0, 0, 0));
         return "index";
     }
 }
