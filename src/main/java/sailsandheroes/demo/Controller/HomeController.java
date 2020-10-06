a76c001efc3ba72ba565ccdeb1b1cba593573ed0
BenjiHasCode <57215161+BenjiHasCode@users.noreply.github.com>
2020-10-06 11:23:43 +0200

    Added to model tgymel

--- src/main/java/sailsandheroes/demo/Controller/HomeController.java
+++ src/main/java/sailsandheroes/demo/Controller/HomeController.java
@@ -5,11 +5,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import sailsandheroes.demo.Model.Hex;
 
+import java.awt.*;
+
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute(new Hex(new Point(100, -100)));
         return "index";
     }
 }
