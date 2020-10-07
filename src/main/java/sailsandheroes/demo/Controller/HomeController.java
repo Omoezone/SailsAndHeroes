fcc36a0eff5e9000084611181aae1d23dbb26316
joal <jokke34@gmail.com>
2020-10-07 22:19:18 +0200

    Javascript sends array of points instead of single point.

--- src/main/java/sailsandheroes/demo/Controller/HomeController.java
+++ src/main/java/sailsandheroes/demo/Controller/HomeController.java
@@ -6,11 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import sailsandheroes.demo.Model.Board;
-import sailsandheroes.demo.Model.Hex;
 import sailsandheroes.demo.Model.Ship;
 
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Controller
@@ -25,6 +25,7 @@ public class HomeController {
         Ship myShip = new Ship();
         myShip.setId(1);
         myShip.setStartPos(new Point(1, 1));
+        myShip.setSpeed(5);
 
         model.addAttribute("ship", myShip);
         model.addAttribute("list", hexboard.getHexGrid());
@@ -34,14 +35,25 @@ public class HomeController {
 
     @PostMapping("/movement")
     public String movement(@RequestBody String data) {
-        data = data.replace("=", "");
+        /*data = data.replace("=", "");
         String[] items = data.split("\\+");
 
         int x = Integer.parseInt(items[0]);
         int y = Integer.parseInt(items[3]);
         int shipId = Integer.parseInt(items[6]);
 
-        Point point = new Point(x, y);
+        Point point = new Point(x, y);*/
+        String[] test = data.split(",");
+        List<Point> myPoints = new ArrayList<>();
+        int shipId = Integer.parseInt(test[0]);
+        for (int i = 1, j = 2; i < test.length; i+=2) {
+            int x = Integer.parseInt(test[i]);
+            int y = Integer.parseInt(test[j]);
+            myPoints.add(new Point(x, y));
+            j += 2;
+        }
+        System.out.println("Id of ship is: " + shipId);
+        System.out.println(myPoints);
         return "redirect:/test";
     }
 }
