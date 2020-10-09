ce3984f19d6a2df384e1a49397f16d3841d35d10
JoakimGit <jokke34@gmail.com>
2020-10-09 09:16:08 +0200

    Small change.

--- src/main/java/sailsandheroes/demo/Controller/HomeController.java
+++ src/main/java/sailsandheroes/demo/Controller/HomeController.java
@@ -1,15 +1,10 @@
 package sailsandheroes.demo.Controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import sailsandheroes.demo.Service.ShipService;
 
 @Controller
 public class HomeController {
-
-    @Autowired
-    ShipService shipService;
 
     @GetMapping("/")
     public String index() {
