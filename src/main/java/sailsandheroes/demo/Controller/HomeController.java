dc028c17c106d337474135dcc4561693d32df3bc
BenjiHasCode <57215161+BenjiHasCode@users.noreply.github.com>
2020-10-09 12:57:14 +0200

    Send data for shoot down with rest of path array

--- src/main/java/sailsandheroes/demo/Controller/HomeController.java
+++ src/main/java/sailsandheroes/demo/Controller/HomeController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import sailsandheroes.demo.Model.Board;
 import sailsandheroes.demo.Model.Hex;
 import sailsandheroes.demo.Model.Ship;
@@ -65,6 +66,10 @@ public class HomeController {
         }
         System.out.println("Id of ship is: " + shipId);
         System.out.println(myPoints);
+
+        //TODO SHOOT TEST
+        System.out.println("I recieved this shoot coordinate: " + test[test.length-2] + ", " + test[test.length-1]);
+
         return "redirect:/test";
     }
 }
